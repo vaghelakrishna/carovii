0fe572d5525733b0921461fc74e50ac305d96799
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-07-09 22:47:29 +0530

    fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// import { WorkspaceProvider } from "./context/WorkspaceContext";
+
+ReactDOM.createRoot(
+  document.getElementById("root")!
+).render(
+  <React.StrictMode>
+    {/* <WorkspaceProvider> */}
+      <App />
+    {/* </WorkspaceProvider> */}
+  </React.StrictMode>
+);
