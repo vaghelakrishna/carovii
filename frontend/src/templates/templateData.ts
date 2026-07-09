7856f0c9f4a1f9a8533a976f5b484d338594081d
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-07-10 00:39:47 +0530

    fix

---
+++ frontend/src/templates/templateData.ts
@@ -0,0 +1,40 @@
+export const templates = [
+  {
+    id: 1,
+    username: "krishnacreates.design",
+    profile: "./src/assets/images/cutie.png",
+    image: "./src/assets/images/cutie.png",
+    caption: "Minimal Jewelry Carousel ✨",
+    likes: "1,284",
+    url: "https://instagram.com/",
+  },
+  {
+    id: 2,
+    username: "krishnacreates.design",
+    profile: "./src/assets/images/cutie.png",
+    image: "/templates/template2.webp",
+    caption: "Modern Fashion Carousel 🤍",
+    likes: "942",
+    url: "https://instagram.com/",
+  },
+  {
+    id: 3,
+    username: "krishnacreates.design",
+    profile: "./src/assets/images/cutie.png",
+    image: "/templates/template3.webp",
+    caption: "Travel Story Template 🌎",
+    likes: "2,103",
+    url: "https://instagram.com/",
+  },
+  // {
+  //   id: 4,
+  //   username: "krishnacreates.design",
+  //   profile: "./src/assets/images/cutie.png",
+  //   image: "/templates/template4.webp",
+  //   caption: "Business Carousel 🚀",
+  //   likes: "852",
+  //   url: "https://instagram.com/",
+  // },
+
+
+];
