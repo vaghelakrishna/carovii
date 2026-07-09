5de3f9a873617e1dfb0272dc60d192f960089d62
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-07-09 13:39:48 +0530

    fix

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/layout/Navbar";
+import Hero from "../components/hero/Hero";
+import Footer from "../components/layout/Footer";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
