06f19a6af70d38428ca7d8b7e79ee09ad556ed1e
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-07-09 22:47:58 +0530

    fix

--- frontend/src/components/workspace/Workspace.tsx
+++ frontend/src/components/workspace/Workspace.tsx
@@ -1,38 +1,26 @@
-import UploadCard from "./UploadCard";
-import SettingsCard from "./SettingsCard";
-import PreviewCard from "./PreviewCard";
-import ExportCard from "./ExportCard";
+import UploadPanel from "./UploadPanel";
+import SettingsPanel from "./SettingsPanel";
+import PreviewPanel from "./PreviewPanel";
+
 import Container from "../layout/Container";
 
 const Workspace = () => {
   return (
     <section className="pb-24">
       <Container>
+        <div className="rounded-[32px] border border-[#ECEAF3] bg-white p-8 shadow-[0_20px_60px_rgba(15,23,42,.08)]">
 
-        <div className="rounded-[34px] border border-slate-200 bg-white p-8 shadow-[0_30px_100px_rgba(15,23,42,.08)]">
+          <div className="grid gap-8 xl:grid-cols-[340px_300px_minmax(0,1fr)]">
 
-          <div className="grid gap-8 lg:grid-cols-12">
+            <UploadPanel />
 
-            <div className="space-y-6 lg:col-span-4">
+            <SettingsPanel />
 
-              <UploadCard />
-
-              <SettingsCard />
-
-            </div>
-
-            <div className="space-y-6 lg:col-span-8">
-
-              <PreviewCard />
-
-              <ExportCard />
-
-            </div>
+            <PreviewPanel />
 
           </div>
 
         </div>
-
       </Container>
     </section>
   );
