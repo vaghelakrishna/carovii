1fc332c16b8cfca0e9dc9a26912a84f27122b6ed
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-07-10 12:39:24 +0530

    fix

--- frontend/src/templates/FreeTemplates.tsx
+++ frontend/src/templates/FreeTemplates.tsx
@@ -1,22 +1,68 @@
 import { ArrowRight } from "lucide-react";
 import TemplateCard from "./TemplateCard";
 import { templates } from "./templateData";
+import { useRef, useState } from "react";
 
 const FreeTemplates = () => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  
+  const [isDown, setIsDown] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(0);
+  
+  const handleMouseDown = (e: React.MouseEvent) => {
+    if (!scrollRef.current) return;
+  
+    setIsDown(true);
+    setStartX(e.pageX - scrollRef.current.offsetLeft);
+    setScrollLeft(scrollRef.current.scrollLeft);
+  };
+  
+  const handleMouseLeave = () => {
+    setIsDown(false);
+  };
+  
+  const handleMouseUp = () => {
+    setIsDown(false);
+  };
+  
+  const handleMouseMove = (e: React.MouseEvent) => {
+    if (!isDown || !scrollRef.current) return;
+  
+    e.preventDefault();
+  
+    const x = e.pageX - scrollRef.current.offsetLeft;
+    const walk = (x - startX) * 1.8; // drag speed
+    scrollRef.current.scrollLeft = scrollLeft - walk;
+  };
   return (
-    <section className="py-24">
+    <section className="py-24 px-20">
 
       <div className="mb-10 flex items-end justify-between">
 
-        <div>
+        <div className="max-w-2xl">
 
           <span className="rounded-full bg-violet-100 px-4 py-2 text-sm font-semibold text-violet-700">
             Free Resources
           </span>
 
-          <h2 className="mt-5 text-5xl font-black text-slate-900">
-            Free Instagram Templates
-          </h2>
+          <div className="inline-flex flex-col items-center mt-2">
+            <h2 className="text-5xl font-black tracking-tight text-slate-900">
+              Free Instagram Templates
+            </h2>
+
+            <img
+              src="./src/assets/illustations/arrow.png" // apna path yaha do
+              alt=""
+              className="
+      ml-85
+      w-[220px]
+      select-none
+      pointer-events-none
+      opacity-90
+    "
+            />
+          </div>
 
           <p className="mt-4 max-w-2xl text-lg text-slate-500">
             Browse beautiful carousel templates.
@@ -51,27 +97,33 @@ const FreeTemplates = () => {
 
       </div>
 
-      {/* Scroll */}
-
       <div
+        ref={scrollRef}
+        onMouseDown={handleMouseDown}
+        onMouseLeave={handleMouseLeave}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
         className="
-        flex
-        gap-7
-        overflow-x-auto
-        pb-4
-        scroll-smooth
-        snap-x
-        snap-mandatory
-        "
+    flex
+    gap-7
+    overflow-x-auto
+    overflow-y-hidden
+    pb-4
+    scroll-smooth
+    snap-x
+    snap-mandatory
+    cursor-grab
+    active:cursor-grabbing
+    select-none
+    scrollbar-hide overflow-x-hidden
+  "
       >
         {templates.map((template) => (
           <div
             key={template.id}
-            className="snap-start"
+            className="snap-start shrink-0"
           >
-            <TemplateCard
-              template={template}
-            />
+            <TemplateCard template={template} />
           </div>
         ))}
       </div>
