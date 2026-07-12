44fd049cf83204526b3388c17dbc07a63d511f03
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-07-12 23:33:50 +0530

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,60 +2,68 @@ import Hero from "./components/hero/Hero";
 import UploadPanel from "./components/workspace/UploadPanel";
 import SettingsPanel from "./components/workspace/SettingsPanel";
 import PreviewPanel from "./components/workspace/PreviewPanel";
-// import WhatsNewCarousel from "./components/workspace/WhatsNewCarousel"
 import FreeTemplates from "./templates/FreeTemplates";
 import Footer from "./components/layout/Footer";
 import FAQSection from "./components/layout/FAQSection";
+
+import { useWorkspaceStore } from "./store/workspaceStore";
+
 function App() {
+  const { preview } = useWorkspaceStore();
+
   return (
-    <div className="min-h-screen bg-[#FAF9FF] overflow-x-hidden">
+    <div className="min-h-screen overflow-x-hidden bg-[#FAF9FF]">
 
       {/* Background */}
       <div className="pointer-events-none fixed inset-0 -z-10 overflow-hidden">
-        <div className="absolute left-1/2 top-0 h-[500px] w-[500px] -translate-x-1/2 rounded-full bg-violet-300/20 blur-[120px] md:h-[650px] md:w-[650px] md:blur-[160px]" />
+        <div className="absolute left-1/2 top-0 h-[650px] w-[650px] -translate-x-1/2 rounded-full bg-violet-300/20 blur-[160px]" />
 
-        <div className="absolute bottom-0 right-0 h-[300px] w-[300px] rounded-full bg-pink-300/20 blur-[90px] md:h-[500px] md:w-[500px] md:blur-[140px]" />
+        <div className="absolute bottom-0 right-0 h-[500px] w-[500px] rounded-full bg-pink-300/20 blur-[140px]" />
 
-        <div className="absolute left-0 top-1/2 h-[250px] w-[250px] rounded-full bg-sky-300/20 blur-[80px] md:h-[450px] md:w-[450px] md:blur-[140px]" />
+        <div className="absolute left-0 top-1/2 h-[450px] w-[450px] rounded-full bg-sky-300/20 blur-[140px]" />
       </div>
 
       <Hero />
 
-      <main className="mx-auto max-w-7xl px-4 pb-16 sm:px-6 lg:px-8 lg:pb-24">
+      <main className="mx-auto max-w-7xl px-4 pb-20 sm:px-6 lg:px-8">
 
-        <div
-          className="
-            grid
-            gap-6
+        {/* BEFORE UPLOAD */}
 
-            grid-cols-1
-
-            lg:grid-cols-2
-
-            xl:grid-cols-3
-          "
-        >
-          {/* Upload */}
-          <div className="order-1">
+        {!preview ? (
+          <div className="mx-auto max-w-3xl">
             <UploadPanel />
           </div>
+        ) : (
+          /* AFTER UPLOAD */
 
-          {/* Settings */}
-          <div className="order-2">
-            <SettingsPanel />
+          <div
+            className="
+              grid
+              gap-6
+              grid-cols-1
+              lg:grid-cols-2
+              xl:grid-cols-3
+            "
+          >
+            <div>
+              <UploadPanel />
+            </div>
+
+            <div>
+              <SettingsPanel />
+            </div>
+
+            <div className="lg:col-span-2 xl:col-span-1">
+              <PreviewPanel />
+            </div>
           </div>
-
-          {/* Preview */}
-          <div className="order-3 xl:col-span-1 lg:col-span-2">
-            <PreviewPanel />
-          </div>
-
-        </div>
-
+        )}
       </main>
+
       <FreeTemplates />
-      {/* <WhatsNewCarousel /> */}
+
       <FAQSection />
+
       <Footer />
     </div>
   );
