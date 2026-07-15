02920f586a7496a887fc1cdddf0e60cf20a2bbb1
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-07-15 16:34:20 +0530

    fix

--- frontend/src/components/workspace/VideoMaker/generateVideo.ts
+++ frontend/src/components/workspace/VideoMaker/generateVideo.ts
@@ -33,7 +33,7 @@ export async function generateVideo({
   // ---------- Canvas ----------
 
   const canvas = document.createElement("canvas");
-  const ctx = canvas.getContext("2d");
+  const ctx = canvas.getContext("2d")!;
 
   if (!ctx) {
     throw new Error("Canvas not supported");
