ed41c0b15cb7ad3a63c27fca6c0b92d69939d412
vaghelakrishna <vaghelakrishna523@gmail.com>
2026-07-16 13:16:07 +0530

    fix

--- frontend/src/utils/download.ts
+++ frontend/src/utils/download.ts
@@ -3,28 +3,11 @@ import { saveAs } from "file-saver";
 
 export const downloadZip = async (images: string[]) => {
   const zip = new JSZip();
-
-  for (let i = 0; i < images.length; i++) {
-    const response = await fetch(images[i]);
-    const blob = await response.blob();
-
-    zip.file(`slide-${i + 1}.png`, blob);
-  }
-
-  const content = await zip.generateAsync({
-    type: "blob",
-  });
-
-  saveAs(content, "carovii-carousel.zip");
-};
-
-export const downloadSingle = async (
-  image: string,
-  index: number
-) => {
-  const response = await fetch(image);
-
-  const blob = await response.blob();
-
-  saveAs(blob, `slide-${index + 1}.png`);
+  await Promise.all(
+    images.map(async (url, i) => {
+      const blob = await fetch(url).then((r) => r.blob());
+      zip.file(`slide-${i + 1}.png`, blob);
+    })
+  );
+  saveAs(await zip.generateAsync({ type: "blob" }), "carovii-carousel.zip");
 };
